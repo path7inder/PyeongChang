41766335c04d64ddd111d74938e3639aab50acc5
path7inder <path7inder@gmail.com>
2018-04-21 22:27:39 +0900

    Add functions convert session info dict and price dict to array and merge

--- PyeongChang.py
+++ PyeongChang.py
@@ -20,7 +20,7 @@ def get_session_list():
 	return res
 
 def get_session_info(session_code):
-	url = "https://ticketapi.pyeongchang2018.com/api/Session/GetSession/?Culture=ko-kr&SessionCode=" + session_code
+	url = "https://ticketapi.pyeongchang2018.com/api/Session/GetSession/?Culture=ko-kr&SessionCode=" + session_code	    
 	req = requests.get(url)
 	res = req.json()
 	return res	
@@ -31,9 +31,27 @@ def get_price_list(session_code):
 	res = req.json()["data"]
 	return res
 
+def get_session_info_row(session_info):
+	keys = ["DisciplineName", "EventName", "StartDate", "StartTime", "EndTime", "ClusterName", "VenueName"]
+	row = []
+	for key in keys:
+		row.append(session_info[key].replace("\n","").strip())		
+	return row
+
+def get_price_list_row(price_list):	
+	keys = ["SalesPrice", "GP_SeatCount"]
+	row = []
+	for price in price_list:		
+		if price["PriceName"] == u"일반":
+			for key in keys:
+				row.append(str(price[key]).replace("\n","").strip())
+	return row	
+
 session_list = get_session_list()
 for session in session_list:
-	session_code = session["SessionCode"]
+	session_code = session["SessionCode"]		
 	session_info = get_session_info(session_code)
-	price_list = get_price_list(session_code);	
-	print price_list
+	price_list = get_price_list(session_code)
+	row = get_session_info_row(session_info)	
+	row.extend(get_price_list_row(price_list))
+	print row
